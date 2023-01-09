baf8f9a4df4cf5f757372927c3bb7c60bc85cd99
jiraporn <janejennie042@gmail.com>
2023-01-09 12:13:17 +0700

    1

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+
+    <form action="show.php">
+        What is your name:<input type = "text" name = "name"><br>
+        What is your age:<input type = "text" name = "age"><br>
+        What is your gender:<input type = "text" name = "gender"><br>
+        What is your marriage status:<input type = "text" name = "marriage_status"><br>
+        <input type = "submit" value = "sumit">
+    </form>
+
+</body>
+</html>
